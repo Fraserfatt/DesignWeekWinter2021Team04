689b08d1151b59a3f5a25c49aa4d8d05f56bde85
fraser13 <fraser13@sheridancollege.ca>
2021-03-08 17:54:02 -0500

    Import polyverse sky with faux daynight cycle prefab

---
+++ DesignWeekWinter2021Team04/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    PolyverseSkiesType sky;
+    int multiplier = 1;
+    Material skyboxDay, skyboxNightSlot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sky = GetComponent<PolyverseSkiesType>();
+        skyboxDay = sky.skyboxDay;
+        skyboxNightSlot = sky.skyboxNight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sky.updateLighting)
+        {
+            DynamicGI.UpdateEnvironment();
+        }
+
+        sky.timeOfDay += sky.speed * Time.deltaTime * multiplier;
+
+        if (sky.timeOfDay > 1 || sky.timeOfDay < 0)
+        {
+            multiplier *= -1;
+        }
+    }
+}
