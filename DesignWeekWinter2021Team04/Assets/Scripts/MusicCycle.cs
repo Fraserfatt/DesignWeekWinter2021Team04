0e87545a87aa4e33921e59580dad14d6f638e484
Cody Beinert <codybeinert123@gmail.com>
2021-03-09 11:33:46 -0500

    Make an Audio Player object and cause ambient music to play and loop infinitely during gameplay

---
+++ DesignWeekWinter2021Team04/Assets/Scripts/MusicCycle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicCycle : MonoBehaviour
+{
+
+    public AudioSource ambient;
+    
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        ambient = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        ambient.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
