f6b3094e6cc01088bbc1c81d56e061ccf0fac313
fraser13 <fraser13@sheridancollege.ca>
2021-03-09 12:21:57 -0500

    Implement dummy interactability

---
+++ DesignWeekWinter2021Team04/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/DummyInteractable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyInteractable : MonoBehaviour, IInteractable
+{
+
+    //Write what the interactable will do
+    public void Interact()
+    {
+        Debug.Log("You interacted with me! at: " + Time.time);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DesignWeekWinter2021Team04/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/InteractController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractController : MonoBehaviour
+{
+    [SerializeField]
+    float interactionDistance;
+
+    [SerializeField]
+    GameObject interactTextPrompt;
+
+    Camera cameraComponent;
+
+    GameObject currentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraComponent = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        #region original raycast code in this region
+        //if (Physics.Raycast(cameraComponent.transform.position, cameraComponent.transform.forward, out hit, interactionDistance, LayerMask.GetMask("Interactable")))
+        //{
+        //    Debug.Log("Highlighting " + hit.collider.name + " at time: " + Time.time);
+        //    interactTextPrompt.SetActive(true);
+        //    interactTextPrompt.transform.position = cameraComponent.WorldToScreenPoint(hit.point);
+        //}
+        //else
+        //{
+        //    currentObject = null;
+        //    Debug.Log("Currently not highlighting anything at time: " + Time.time);
+        //    interactTextPrompt.SetActive(false);
+        //}
+        #endregion
+
+        //Current cast to check for interaction.  If true, collects current object and highlights via UI
+        if (Physics.SphereCast(cameraComponent.transform.position, 0.5f, cameraComponent.transform.forward, out hit, interactionDistance, LayerMask.GetMask("Interactable")))
+        {
+            interactTextPrompt.SetActive(true);
+            //Debug.Log("Highlighting " + hit.collider.name + " at time: " + Time.time);
+            interactTextPrompt.transform.position = cameraComponent.WorldToScreenPoint(hit.point);
+            currentObject = hit.transform.gameObject;
+        }
+        else
+        {
+            interactTextPrompt.SetActive(false);
+            //Debug.Log("Currently not highlighting anything at time: " + Time.time);
+            currentObject = null;
+        }
+
+        if (currentObject != null && Input.GetKeyDown(KeyCode.E))
+        {
+            currentObject.GetComponent<IInteractable>().Interact();
+        }
+    }
+}
