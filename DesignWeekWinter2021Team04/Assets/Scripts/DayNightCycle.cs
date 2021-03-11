7dcee9183ed82902ba78d0ddc34b2f3d4ca90ddd
fraser13 <fraser13@sheridancollege.ca>
2021-03-11 13:14:09 -0500

    Complete scene rework and add control layout to loading

--- DesignWeekWinter2021Team04/Assets/Scripts/DayNightCycle.cs
+++ DesignWeekWinter2021Team04/Assets/Scripts/DayNightCycle.cs
@@ -8,12 +8,18 @@ public class DayNightCycle : MonoBehaviour
     int multiplier = 1;
     Material skyboxDay, skyboxNightSlot;
 
+    Light directionalLight;
+
+    [SerializeField]
+    Color dayColour, nightColour;
+
     // Start is called before the first frame update
     void Start()
     {
         sky = GetComponent<PolyverseSkiesType>();
         skyboxDay = sky.skyboxDay;
         skyboxNightSlot = sky.skyboxNight;
+        directionalLight = GetComponentInChildren<Light>();
     }
 
     // Update is called once per frame
@@ -25,10 +31,12 @@ public class DayNightCycle : MonoBehaviour
         }
 
         sky.timeOfDay += sky.speed * Time.deltaTime * multiplier;
+        directionalLight.color = Color.Lerp(dayColour, nightColour, sky.timeOfDay);
 
         if (sky.timeOfDay > 1 || sky.timeOfDay < 0)
         {
             multiplier *= -1;
         }
     }
+
 }
